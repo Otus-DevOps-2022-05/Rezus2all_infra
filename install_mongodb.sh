3bed10dbe239c6face34f7c68fc94038333acc7c
Akhmet Shabakaev <ashabakaev@rencredit.ru>
2022-07-26 18:29:55 +0300

    Summary

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Установка MongoDB
 $ sudo apt-get update
-$ sudo apt-get install -y mongodb-org
+$ sudo apt-get install -y mongodb
 #Запускаем MongoDB:
-$ sudo systemctl start mongod
+$ sudo systemctl start mongodb
 #Добавляем в автозапуск:
-$ sudo systemctl enable mongod
+$ sudo systemctl enable mongodb
