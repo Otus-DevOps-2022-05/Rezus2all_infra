f44cf1e4fc4ec1009091d9ca3f2df67b45821962
Akhmet Shabakaev <ashabakaev@rencredit.ru>
2022-08-23 22:53:36 +0300

    summary 230822

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -3,8 +3,8 @@ rm /var/lib/apt/lists/lock
 rm /var/cache/apt/archives/lock
 rm /var/lib/dpkg/lock*
 
-apt update
-apt install -y mongodb-server
+apt-get update
 sleep 60
+apt-get install -y mongodb-server
 systemctl enable mongodb
 systemctl start mongodb
